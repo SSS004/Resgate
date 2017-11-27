c1c1c73dea9564d6ae2ed6c25767240abbacd53c
Silvio Shindi Shimizu <silviosshimizu@gmail.com>
2017-11-27 16:59:18 -0200

    Initial Commit

---
+++ Resgate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Resgate
+//
+//  Created by Silvio Shindi Shimizu on 27/11/2017.
+//  Copyright © 2017 Cinco Mais Tecnologia em Saúde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
