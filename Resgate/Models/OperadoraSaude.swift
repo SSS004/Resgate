e04aca169c9d9bd830db0764bc544082e05c909e
Silvio Shindi Shimizu <silviosshimizu@gmail.com>
2017-12-07 18:15:04 -0200

    Atualização

---
+++ Resgate/Models/OperadoraSaude.swift
@@ -0,0 +1,21 @@
+//
+//  OperadoraSaude.swift
+//  Resgate
+//
+//  Created by Silvio Shindi Shimizu on 06/12/2017.
+//  Copyright © 2017 Cinco Mais Tecnologia em Saúde. All rights reserved.
+//
+
+import UIKit
+
+class OperadoraSaude {
+  
+  public var nomeOperadora: String
+  public var dataElegibilidade: Date
+  
+  init() {
+    nomeOperadora = ""
+    dataElegibilidade = Date()
+  }
+  
+}
